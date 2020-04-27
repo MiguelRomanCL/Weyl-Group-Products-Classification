f115e70ac9148d3dcab5af9b6bc6e2377593cc32
Miguel Román <mnroman@uc.cl>
2020-04-27 02:38:46 -0400

    type Group defined (starting)

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,59 +2,78 @@ module Lib
     ( someFunc
     ) where
 
+import Data.List (nub, sort) 
+
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 
-import Data.List (nub, sort) 
+
+type Cardinality = Integer
+type SumDimIrreps = Integer
+data Orders = Orders [Integer] deriving (Show)
+data Group = Group Cardinality SumDimIrreps Orders deriving (Show)
 
 
-ordersC3= map (fromIntegral) ([2,3,4,6]);
-ordersC4= map (fromIntegral) ([2,3,4,6,8]);
-ordersC5= map (fromIntegral) ([2,3,4,5,6,8,10,12]);
-ordersC6= map (fromIntegral) ([2,3,4,5,6,8,10,12]);
-ordersD3= map (fromIntegral) ([2,3,4]);
-ordersD4= map (fromIntegral) ([2,3,4,6]);
-ordersD5= map (fromIntegral) ([2,3,4,5,6,8,12]);
-ordersD6= map (fromIntegral) ([2,3,4,5,6,8,10,12]);
-ordersD7= map (fromIntegral) ([2,3,4,5,6,7,8,10,12,20,24]);
-ordersS3= map (fromIntegral) ([2,3]);
-ordersS4= map (fromIntegral) ([2,3,4]);
-ordersS5= map (fromIntegral) ([2,3,4,5,6]);
-ordersS6= map (fromIntegral) ([2,3,4,5,6]);
-ordersS7= map (fromIntegral) ([2,3,4,5,6,7,10,12]);
-ordersS8= map (fromIntegral) ([2,3,4,5,6,7,8,10,12,15]);
-ordersF4= map (fromIntegral) ([2,3,4,6,8,12]);
-ordersG2= map (fromIntegral) ([2,3,6]);
-ordersE6= map (fromIntegral) ([2,3,4,5,6,8,9,10,12]);
-ordersE7= map (fromIntegral) ([2,3,4,5,6,7,8,9,10,12,14,15,18,30]);
-ordersE8= map (fromIntegral) ([2,3,4,5,6,7,8,9,10,12,14,15,18,20,24,30]);
 
 
-gC3= (ordersC3, 20,48);
-gC4= (ordersC4, 76,384);
-gC5= (ordersC5, 312,3840);
-gC6= (ordersC6, 1384,46080);
-gD3= (ordersD3, 10,24);
-gD4= (ordersD4, 44,192);
-gD5= (ordersD5, 156,1920);
-gD6= (ordersD6, 752,23040);
-gD7= (ordersD7, 3256,322560);
-gF4= (ordersF4, 140,1152);
-gG2= (ordersG2, 8,12);
-gS3= (ordersS3, 4,6);
-gS5= (ordersS5, 26,120);
-gS6= (ordersS6, 76,720);
-gS7= (ordersS7, 232,5040);
-gE6= (ordersE6, 892,51840);
-gE7= (ordersE7, 10208,2903040);
-gE8= (ordersE8, 199952,696729600);
+oC3 = Orders [2, 3, 4, 6]
+oC4 = Orders [2, 3, 4, 6, 8]
+oC5 = Orders [2, 3, 4, 5, 6, 8, 10, 12]
+oC6 = Orders [2, 3, 4, 5, 6, 8, 10, 12]
+oD3 = Orders [2, 3, 4]
+oD4 = Orders [2, 3, 4, 6]
+oD5 = Orders [2, 3, 4, 5, 6, 8, 12]
+oD6 = Orders [2, 3, 4, 5, 6, 8, 10, 12]
+oD7 = Orders [2, 3, 4, 5, 6, 7, 8, 10, 12, 20, 24]
+oS3 = Orders [2, 3]
+oS4 = Orders [2, 3, 4]
+oS5 = Orders [2, 3, 4, 5, 6]
+oS6 = Orders [2, 3, 4, 5, 6]
+oS7 = Orders [2, 3, 4, 5, 6, 7, 10, 12]
+oS8 = Orders [2, 3, 4, 5, 6, 7, 8, 10, 12, 15]
+oF4 = Orders [2, 3, 4, 6, 8, 12]
+oG2 = Orders [2, 3, 6]
+oE6 = Orders [2, 3, 4, 5, 6, 8, 9, 10, 12]
+oE7 = Orders [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 18, 30]
+oE8 = Orders [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 18, 20, 24, 30]
+
+gC3 = Group        48     20 oC3
+gC4 = Group       384     76 oC4
+gC5 = Group      3840    312 oC5
+gC6 = Group     46080   1384 oC6
+gD3 = Group        24     10 oD3
+gD4 = Group       192     44 oD4
+gD5 = Group      1920    156 oD5
+gD6 = Group     23040    752 oD6
+gD7 = Group    322560   3256 oD7
+gF4 = Group      1152    140 oF4
+gG2 = Group        12      8 oG2
+gS3 = Group         6      4 oS3
+gS5 = Group       120     26 oS5
+gS6 = Group       720     76 oS6
+gS7 = Group      5040    232 oS7
+gE6 = Group     51840    892 oE6
+gE7 = Group   2903040  10208 oE7
+gE8 = Group 696729600 199952 oE8
+
+
 
 sord a = sort(nub(a))
 
-cart :: Integral a => [[a]] -> [a]
-cart [] = []
-cart [x] = x
-cart [a,b] = sord [ lcm x y | x <- a , y<- b]
-cart (x:xs) = cart [x,(cart xs)]
+cartord :: Integral a => [[a]] -> [a]
+cartord [] = []
+cartord [x] = x
+cartord [xs,ys] = sord [ lcm x y | x <- xs , y<- ys]
+cartord (x:xs) = cartord [x,(cartord xs)]
+
+
+cart :: [Group] -> Group
+cart []= undefined
+cart [g]= g
+cart [(Group gc gn (Orders go)),(Group hc hn (Orders ho))]= Group (gc*hc) (gn*hn) (Orders (cartord [go,ho]))
+cart (g:gs) = cart [g,(cart gs)]
+
+
 
